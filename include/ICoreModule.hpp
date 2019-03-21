c5b3ba9dea24d580aff58b4261a6194fffa607b2
Gautier de Lataillade <gautier2406@gmail.com>
2019-03-21 17:36:35 +0100

    Add ICoreModule

---
+++ include/ICoreModule.hpp
@@ -0,0 +1,29 @@
+/*
+** EPITECH PROJECT, 2019
+** OOP_arcade_2018
+** File description:
+** ICoreModule
+*/
+
+#ifndef ICOREMODULE_HPP_
+    #define ICOREMODULE_HPP_
+
+#include <vector>
+#include "Entity.hpp"
+#include "Inputs.hpp"
+
+class ICoreModule {
+	public:
+		ICoreModule();
+		virtual ~ICoreModule() = default;
+
+        // virtual void storeGameEntity(AEntity *entity) = 0;
+        // virtual AEntity &getEntity(std::string name) = 0;
+        // virtual std::vector<enum gameInputs> getInputs() = 0;
+        // virtual void renderAll() = 0;
+
+	protected:
+	private:
+};
+
+#endif /* !ICOREMODULE_HPP_ */
