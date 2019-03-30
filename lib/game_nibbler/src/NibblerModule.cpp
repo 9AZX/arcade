04d38ad0487d7e8e953670548f90abca7c69b417
Jon Ihlas <jon.ihlas-mouesca@epitech.eu>
2019-03-31 00:34:16 +0100

    Add move forward in nibbler

--- lib/game_nibbler/src/NibblerModule.cpp
+++ lib/game_nibbler/src/NibblerModule.cpp
@@ -48,9 +48,10 @@ void NibblerModule::play() {
     event = this->_core->getInputs();
     if (event.keys.size() > 0) {
       this->computeInput(event.keys);
-      bodyMove();
-      checkApple();
     }
+    forward();
+    bodyMove();
+    checkApple();
   }
 }
 
@@ -64,34 +65,50 @@ const GameMap &NibblerModule::getMap() const noexcept { return this->_map; }
 
 void NibblerModule::computeInput(std::vector<enum gameInputs> keys) {
   std::pair<int, int> playerPos = this->_core->getEntity(0).getPos();
+  auto ch = keys.back();
 
-  if (keys.back() == RIGHT) {
-    this->_core->getEntity(0).setRotation(90);
-    if (this->checkCollision(
-            std::pair<int, int>(playerPos.first + 1, playerPos.second)))
-      this->_core->getEntity(0).setPos(
-          std::pair<int, int>(playerPos.first + 1, playerPos.second));
-  } else if (keys.back() == LEFT) {
-    this->_core->getEntity(0).setRotation(270);
-    if (this->checkCollision(
-            std::pair<int, int>(playerPos.first - 1, playerPos.second)))
-      this->_core->getEntity(0).setPos(
-          std::pair<int, int>(playerPos.first - 1, playerPos.second));
-  } else if (keys.back() == UP) {
-    this->_core->getEntity(0).setRotation(0);
-    if (this->checkCollision(
-            std::pair<int, int>(playerPos.first, playerPos.second - 1)))
-      this->_core->getEntity(0).setPos(
-          std::pair<int, int>(playerPos.first, playerPos.second - 1));
-  } else if (keys.back() == DOWN) {
-    this->_core->getEntity(0).setRotation(180);
-    if (this->checkCollision(
-            std::pair<int, int>(playerPos.first, playerPos.second + 1)))
-      this->_core->getEntity(0).setPos(
-          std::pair<int, int>(playerPos.first, playerPos.second + 1));
-  } else if (keys.back() == ESCAPE) {
-    std::cout << "You quit the game" << std::endl;
+  switch (ch) {
+    case RIGHT:
+      resetPos();
+      makeMove(std::pair<int, int>(this->_core->getEntity(0).getPos().first + 1,
+                                   this->_core->getEntity(0).getPos().second));
+      this->_core->getEntity(0).setRotation(90);
+      this->_core->getEntity(0).moveRight = true;
+      break;
+    case LEFT:
+      resetPos();
+      makeMove(std::pair<int, int>(this->_core->getEntity(0).getPos().first - 1,
+                                   this->_core->getEntity(0).getPos().second));
+      this->_core->getEntity(0).setRotation(270);
+      this->_core->getEntity(0).moveLeft = true;
+      break;
+    case UP:
+      resetPos();
+      makeMove(
+          std::pair<int, int>(this->_core->getEntity(0).getPos().first,
+                              this->_core->getEntity(0).getPos().second - 1));
+      this->_core->getEntity(0).setRotation(0);
+      this->_core->getEntity(0).moveUp = true;
+      break;
+    case DOWN:
+      resetPos();
+      makeMove(
+          std::pair<int, int>(this->_core->getEntity(0).getPos().first,
+                              this->_core->getEntity(0).getPos().second + 1));
+      this->_core->getEntity(0).setRotation(180);
+      this->_core->getEntity(0).moveDown = true;
+      break;
+    case ESCAPE:
+      std::cout << "You quit the game" << std::endl;
+      break;
   }
+}
+
+void NibblerModule::resetPos() {
+  this->_core->getEntity(0).moveUp = false;
+  this->_core->getEntity(0).moveDown = false;
+  this->_core->getEntity(0).moveLeft = false;
+  this->_core->getEntity(0).moveRight = false;
 }
 
 bool NibblerModule::checkCollision(std::pair<int, int> nextPos) {
@@ -103,16 +120,16 @@ bool NibblerModule::checkCollision(std::pair<int, int> nextPos) {
 void NibblerModule::checkApple() {
   std::pair<int, int> playerPos = this->_core->getEntity(0).getPos();
 
-  for (unsigned int i = 0; i < appleRemain; ++i) {
+  for (unsigned int i = 0; i < appleRemain; i++) {
     if ((playerPos.first == _core->getEntity(i + 100).getPos().first) &&
         (playerPos.second == _core->getEntity(i + 100).getPos().second) &&
         this->_core->getEntity(i + 100).isAlive) {
       snakeWidth++;
       appleRemain--;
       this->_core->getEntity(i + 100).isAlive = false;
-      this->_core->storeGameEntity(
-          new GameEntity('O', "./assets/nibbler/body-snake.png", snakeWidth,
-                         std::make_pair<int, int>(11, 10), false));
+      this->_core->storeGameEntity(new GameEntity(
+          'O', "./assets/nibbler/body-snake.png", snakeWidth,
+          this->_core->getEntity(snakeWidth - 1).getPos(), false));
     }
   }
 }
@@ -126,4 +143,25 @@ void NibblerModule::bodyMove() {
   if (this->_core->getEntity(snakeWidth).getPrevPos().first != -1)
     this->_core->getEntity(99).setPos(
         this->_core->getEntity(snakeWidth).getPrevPos());
+}
+
+void NibblerModule::makeMove(std::pair<int, int> pos) {
+  if (this->checkCollision(pos)) this->_core->getEntity(0).setPos(pos);
+}
+
+void NibblerModule::forward() {
+  AEntity &player = this->_core->getEntity(0);
+  if (player.moveDown) {
+    makeMove(
+        std::pair<int, int>(player.getPos().first, player.getPos().second + 1));
+  } else if (player.moveUp) {
+    makeMove(
+        std::pair<int, int>(player.getPos().first, player.getPos().second - 1));
+  } else if (player.moveLeft) {
+    makeMove(
+        std::pair<int, int>(player.getPos().first - 1, player.getPos().second));
+  } else if (player.moveRight) {
+    makeMove(
+        std::pair<int, int>(player.getPos().first + 1, player.getPos().second));
+  }
 }
